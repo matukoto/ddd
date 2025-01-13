5570653a360dff0883a24ce07e9ce9117c9c08d1
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2025-01-13 17:51:04 +0900

    feat: add fsharp basic

--- function-domain/main.fsx
+++ function-domain/main.fsx
@@ -1,1 +1,11 @@
 printfn "hello"
+// Path: main.fsx
+let name = "world"
+printfn $"hello {name}"
+
+let list = [ 1..10 ]
+let isEven (x: int) : bool = x % 2 = 0
+let evens = list |> List.filter isEven
+List.iter (printf "%i ") evens
+
+[ 1..10 ] |> List.filter isEven |> List.iter (printf "%i ")
